4813bcedce432ff4df2349fb74e4f6449e790a8e
Nate Spilman <nate.spilman@gmail.com>
2021-02-16 13:26:06 -0800

    Styling updates - keyboard looks cool now (#4)
    * Styling updates - keyboard looks cool now
    
    * lowering the brightness on the background

--- src/components/octaveControl.tsx
+++ src/components/octaveControl.tsx
@@ -3,16 +3,25 @@ import { KeyboardContext, UpdateKeyboardContext } from "../hooks/keyboardContext
 import styled from "styled-components";
 
 const StyledOctaveControl = styled.div`
-    height:5rem;
-    width:5rem;
-    background-color:grey;
+display:flex;
+flex-direction:column;
+padding:1rem;
+width:200px;
+align-items:center;
+`
+
+const StyledControlButton = styled.div`
+    height:2rem;
+    width:2rem;
+    background-color:rgb(230,230,230);
     margin:.1rem;
     display:flex;
     align-items:center;
     justify-content:center;
     &:hover{
-        background-color:black;
-        color:green;
+        background-color:rgb(90,20,20);
+        color:white;
+        cursor:pointer;
     }
     &::selection{
         background: transparent;
@@ -38,18 +47,23 @@ function OctaveControl() {
     }
 
     return (
-        <div id="OctaveControl">
-            <StyledOctaveControl className="lower-octave"
+        <StyledOctaveControl id="OctaveControl">
+            <span style = {{fontSize:'1.4rem', color:'rgb(230,230,230)'}}>OCTAVE : {octave}</span>
+            <div className = 'octave-buttons-wrapper'
+                style={{flexDirection:'row', display:'flex'}}
+            >
+            <StyledControlButton className="lower-octave"
                 onClick={() : void => decrementOctave()}
             >
-                Octave -
-            </StyledOctaveControl>
-            <StyledOctaveControl className="upper-octave"
+                 -
+            </StyledControlButton>
+            <StyledControlButton className="upper-octave"
                 onClick={() : void => incrementOctave()}
             >
-                Octave +
-            </StyledOctaveControl>
-        </div>
+                 +
+            </StyledControlButton>
+            </div>
+        </StyledOctaveControl>
     )
 }
 
