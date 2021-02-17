e0ba44734324fb27d1ff2ae71b5d298246edd930
Nate Spilman <nate.spilman@gmail.com>
2021-02-16 21:45:42 -0800

    Look at this cool space background, wow

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import Keyboard from "./components/keyboard"
 
+const coolSpaceBackground = "https://images.unsplash.com/photo-1464802686167-b939a6910659?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1633&q=80";
+
 function App() {
   return (
     <div className="App"
@@ -10,7 +12,8 @@ function App() {
         width: '100vw',
         height: '100vh',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        backgroundImage:`url(${coolSpaceBackground})`
       }}>
       <Keyboard />
     </div>
