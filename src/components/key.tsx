4fce928dbbf56606712c4790a0ffa070d0951906
Nate Spilman <nate.spilman@gmail.com>
2021-02-23 18:40:55 -0800

    Adding event listeners to each key to show playing if playing

--- src/components/key.tsx
+++ src/components/key.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import stopNote from '../hooks/stopNote';
 import playNote from '../hooks/playNote';
 import keyboardToNoteHash from "../data/keyboardToNoteHash"
@@ -9,13 +9,13 @@ import { KeyboardContext } from "../hooks/keyboardContext";
 const StyledKey = styled.div`
     height:10rem;
     width:5rem;
-    background-color:rgb(250,250,250);
+    background-color:${(props:{isPlaying : boolean}) => props.isPlaying ? 'rgb(55,55,55)' : 'rgb(250,250,250)'};
     margin:.1rem;
     display:flex;
     align-items:center;
     justify-content:center;
     &:hover{
-        background-color:rgb(230,230,230);
+        background-color:${(props:{isPlaying : boolean}) => props.isPlaying ? 'rgb(55,55,55)' : 'rgb(230,230,230)'};
         color:rgb(90,20,20);
     }
     &::selection{
@@ -30,8 +30,27 @@ interface KeyProps {
 
 function Key({ note, isMouseDown }: KeyProps) {
     const { audioContextWrapper } = useContext(KeyboardContext)
+    const [isPlaying, setIsPlaying] = useState(false)
   
+    const parseAndPlayKeyCommand = ({key} : KeyboardEvent) => {
+        const triggeredNote = keyboardToNoteHash[key];
+        if(note === triggeredNote){
+            setIsPlaying(true)
+        }
+    }
+
+    const parseAndStopKeyCommand = ({key} : KeyboardEvent) => {
+        const triggeredNote = keyboardToNoteHash[key];
+        if(note === triggeredNote){
+            setIsPlaying(false)
+        }
+    }
+
+    window.addEventListener('keydown', (event) => parseAndPlayKeyCommand(event));
+    window.addEventListener('keyup', (event) => parseAndStopKeyCommand(event));
+
     return (<StyledKey
+        isPlaying = {isPlaying}
         onMouseDown={() => playNote(audioContextWrapper, note )}
         onMouseLeave={() => stopNote(audioContextWrapper, note )}
         onMouseUp={() => stopNote(audioContextWrapper,  note )}
