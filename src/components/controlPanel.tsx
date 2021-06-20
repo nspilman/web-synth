c292ae564996c1b168e18a8e669052b5314f2c7d
Nate Spilman <nate.spilman@gmail.com>
2021-06-20 09:50:03 -0700

    adding StyledDialControl to center dial labels

--- src/components/controlPanel.tsx
+++ src/components/controlPanel.tsx
@@ -1,84 +1,74 @@
-import React, { Dispatch, useState } from 'react';
+import React, { Dispatch, useState } from "react";
 
-import FilterControls from './controls/filterControls';
-import EnvelopeControls from './controls/envelopeControls';
+import FilterControls from "./controls/filterControls";
+import EnvelopeControls from "./controls/envelopeControls";
 import OscControls from "./controls/oscControls";
-import BasicControls from "./controls/basicControls"
-import { useDispatch } from 'react-redux';
-import { AudioControllerAction } from '../store/actions';
+import BasicControls from "./controls/basicControls";
+import { useDispatch } from "react-redux";
+import { AudioControllerAction } from "../store/actions";
 import {
-    StyledControlPanel,
-    StyledBrandLabel,
-    StyledTab,
-    StyledTabButton,
-    StyledTabs
+  StyledControlPanel,
+  StyledBrandLabel,
+  StyledTab,
+  StyledTabButton,
+  StyledTabs,
 } from "./styled/controlPanel/";
 
 enum panelTabs {
-    basic = 'basic',
-    envelope = 'envelope',
-    oscillator = 'oscillator',
-    filter = 'filter'
+  waveform = "waveform",
+  envelope = "envelope",
+  oscillator = "oscillator",
+  filter = "filter",
 }
 
 export type controlState = {
-    triggerStateChange: (newValue: number,
-        stateChangeActionGenerator: (changingValue: number) => AudioControllerAction) => void;
-}
+  triggerStateChange: (
+    newValue: number,
+    stateChangeActionGenerator: (changingValue: number) => AudioControllerAction
+  ) => void;
+};
 
 function ControlPanel() {
-    const dispatch = useDispatch<Dispatch<AudioControllerAction>>();
-    const triggerStateChange = (changingValue: number, actionCreator: (changingValue: number) => AudioControllerAction) => {
-        dispatch(actionCreator(changingValue))
-    }
+  const dispatch = useDispatch<Dispatch<AudioControllerAction>>();
+  const triggerStateChange = (
+    changingValue: number,
+    actionCreator: (changingValue: number) => AudioControllerAction
+  ) => {
+    dispatch(actionCreator(changingValue));
+  };
 
-    const [currentTab, setTab] = useState(panelTabs.basic)
-    const renderCurrentTab = (): JSX.Element => {
-        switch (currentTab) {
-            case panelTabs.basic:
-                return <BasicControls
-                    triggerStateChange={triggerStateChange}
-                />;
-                break;
-            case panelTabs.envelope:
-                return <EnvelopeControls
-                    triggerStateChange={triggerStateChange}
-                />
-                break;
-            case panelTabs.oscillator:
-                return <OscControls
-                    triggerStateChange={triggerStateChange}
-                />
-                break;
-            case panelTabs.filter:
-                return <FilterControls
-                    triggerStateChange={triggerStateChange}
-                />
-            default:
-                return <div>Error Loading Controls</div>
-        }
+  const [currentTab, setTab] = useState(panelTabs.waveform);
+  const renderCurrentTab = (): JSX.Element => {
+    switch (currentTab) {
+      case panelTabs.waveform:
+        return <BasicControls triggerStateChange={triggerStateChange} />;
+      case panelTabs.envelope:
+        return <EnvelopeControls triggerStateChange={triggerStateChange} />;
+      case panelTabs.oscillator:
+        return <OscControls triggerStateChange={triggerStateChange} />;
+      case panelTabs.filter:
+        return <FilterControls triggerStateChange={triggerStateChange} />;
+      default:
+        return <div>Error Loading Controls</div>;
     }
-    return (
-        <StyledControlPanel>
-            <StyledBrandLabel>
-                    N8-JS Web Synth
-            </StyledBrandLabel>
-            <StyledTabs id="tabs">
-                {
-                    Object.keys(panelTabs).map(
-                        tab => <StyledTabButton
-                            key={tab}
-                            isSelected={tab === currentTab}
-                            onClick={() => setTab(tab as panelTabs)}
-                        >{tab}</StyledTabButton>
-                    )
-                }
-            </StyledTabs>
-            <StyledTab>
-                {renderCurrentTab()}
-            </StyledTab>
-        </StyledControlPanel>
-    )
+  };
+  return (
+    <StyledControlPanel>
+      <StyledBrandLabel>N8-JS Web Synth</StyledBrandLabel>
+      <StyledTabs id="tabs">
+        {Object.keys(panelTabs).map((tab) => (
+          <StyledTabButton
+            key={tab}
+            isSelected={tab === currentTab}
+            onClick={() => setTab(tab as panelTabs)}
+          >
+            {tab}
+          </StyledTabButton>
+        ))}
+      </StyledTabs>
+      <StyledTab>{renderCurrentTab()}</StyledTab>
+    </StyledControlPanel>
+  );
 }
 
 export default ControlPanel;
