8a8c6b18d171ddc700e4e9471be8170a0da5c6e9
Nate Spilman <nate.spilman@gmail.com>
2021-06-20 13:17:45 -0700

    adding StyledDialControl to center dial labels (#50)

--- src/components/controls/components/dialControl.tsx
+++ src/components/controls/components/dialControl.tsx
@@ -1,41 +1,34 @@
-
-import React from 'react'
-import { StyledLabel } from "../../styled/control";
+import React from "react";
+import { StyledDialControl, StyledLabel } from "../../styled/control";
 import ControlKnob from "./controlKnob";
-import IDialControlParameters from "../../../interfaces/IDailControlParameters"
+import IDialControlParameters from "../../../interfaces/IDailControlParameters";
 
 type Props = {
-    parameters : IDialControlParameters,
-    value: number,
-    setValue : (value: number) => void;
-}
+  parameters: IDialControlParameters;
+  value: number;
+  setValue: (value: number) => void;
+};
 
-function DialControl(props : Props){
-    const { min, 
-            max, 
-            title,
-            factor = 1
-        } = props.parameters;
-    
-    const { value, setValue } = props;
+function DialControl(props: Props) {
+  const { min, max, title, factor = 1 } = props.parameters;
 
-    const localSetvalue = (value: number) =>{
-         setValue(value / factor)
-    }
+  const { value, setValue } = props;
 
-    return( 
-        <div>
-            <StyledLabel>
-                {title}
-            </StyledLabel>
-            <ControlKnob
-                min={ min * factor }
-                max={ max * factor }
-                value={ value * factor }
-                setValue={ localSetvalue }
-            />
-        </div>
-    )
+  const localSetvalue = (value: number) => {
+    setValue(value / factor);
+  };
+
+  return (
+    <StyledDialControl>
+      <StyledLabel>{title}</StyledLabel>
+      <ControlKnob
+        min={min * factor}
+        max={max * factor}
+        value={value * factor}
+        setValue={localSetvalue}
+      />
+    </StyledDialControl>
+  );
 }
 
 export default DialControl;
--- src/components/styled/control/index.tsx
+++ src/components/styled/control/index.tsx
@@ -1,17 +1,25 @@
-import styled from 'styled-components'
-import colors from "../../../data/colors"
+import styled from "styled-components";
+import colors from "../../../data/colors";
 
 export const StyledLabel = styled.span`
-    color:${colors.blue};
-`
+  color: ${colors.blue};
+  padding-bottom: 4px;
+`;
 
 export const StyledControl = styled.div`
-    display:flex;
-    flex-direction:row;
-    align-items:center;
-    justify-content:space-evenly;
-`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+`;
 
 export const StyledKnob = styled.div`
-    margin:auto;
-`
+  margin: auto;
+`;
+
+export const StyledDialControl = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
